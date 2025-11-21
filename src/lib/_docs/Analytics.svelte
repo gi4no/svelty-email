fecb25fa71448cc24dd9bd21fbc8e563c95a350b
sandro gianola <sandro.gianola@4gifters.com>
2025-11-21 16:57:47 +0100

    fix analytics component

--- src/lib/_docs/Analytics.svelte
+++ src/lib/_docs/Analytics.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
+	import { page } from '$app/state';
 
-	import { page } from '$app/stores';
-
-	run(() => {
+	$effect(() => {
+		console.log('effect');
 		if (typeof gtag !== 'undefined') {
 			gtag('config', 'G-583B58BZRR', {
 				page_title: document.title,
-				page_path: $page.url.pathname
+				page_path: page.url.pathname
 			});
 		}
 	});
